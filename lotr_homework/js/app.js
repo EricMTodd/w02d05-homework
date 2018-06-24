8492c22a85c2cb33f9aa1c0b3fa10a252cdd5e77
-thugnasty- <eterictodd@gmail.com>
2018-06-24 12:58:55 -0600

    Chapter 13 complete - Mordor is destroyed, and the hobbits are safely back in The Shire.

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -317,10 +317,13 @@ const weWantsIt = () => {
 const thereAndBackAgain = () => {
 
   // 1. remove Gollum and the Ring from the DOM
+  $("#gollum").remove();
 
   // 2. remove all the baddies from the DOM
+  $(".baddy").remove();
 
   // 3. Move all the hobbits back to the shire
+  $(".hobbit").appendTo("#The-Shire");
 
 };
 
@@ -353,16 +356,16 @@ $(() => {
 
 });
 
-makeMiddleEarth();
-makeHobbits();
-keepItSecretKeepItSafe();
-makeBaddies();
-makeBuddies();
-leaveTheShire();
-beautifulStranger();
-forgeTheFellowShip();
-theBalrog();
-hornOfGondor();
-itsDangerousToGoAlone();
-weWantsIt();
+// makeMiddleEarth();
+// makeHobbits();
+// keepItSecretKeepItSafe();
+// makeBaddies();
+// makeBuddies();
+// leaveTheShire();
+// beautifulStranger();
+// forgeTheFellowShip();
+// theBalrog();
+// hornOfGondor();
+// itsDangerousToGoAlone();
+// weWantsIt();
 // thereAndBackAgain();
